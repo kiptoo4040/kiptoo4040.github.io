6cba46e637fb9779efb5d316f143dbf58ceb65dc
kiptoo4040 <koechzadoc7@gmail.com>
2022-01-26 19:37:42 +0300

    initial commit

---
+++ introduction.php
@@ -0,0 +1,27 @@
+<?php
+//
+//echo "Welcome to our php first class";
+//echo "<br>";
+//echo "hows your today class";
+//?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+echo "Welcome to our php first class";
+echo "<br>";
+echo "hows your today class";
+?>
+<h2>this is awesome</h2>
+
+</body>
+</html>
